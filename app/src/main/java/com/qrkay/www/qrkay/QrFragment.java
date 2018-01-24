c87099147bf780de81a20c8879160e5d67afeb42
nolanm46-dcu <mark.nolan46@mail.dcu.ie>
2018-01-24 19:04:04 +0000

    Added Tab Layout to landing screen and generic QR code and voucher page

---
+++ app/src/main/java/com/qrkay/www/qrkay/QrFragment.java
@@ -0,0 +1,21 @@
+package com.qrkay.www.qrkaydemo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mark2 on 18/01/2018.
+ */
+
+public class QrFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_qr, container, false);
+    }
+}
---
+++ app/src/main/java/com/qrkay/www/qrkay/VoucherFragment.java
@@ -0,0 +1,19 @@
+package com.qrkay.www.qrkaydemo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mark2 on 19/01/2018.
+ */
+
+public class VoucherFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_voucher, container, false);
+    }
+}
